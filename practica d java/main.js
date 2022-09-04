761e96758176eb892b498b152a554c5bcfdfe65f
ELISONBLK <111245410+ELISONBLK@users.noreply.github.com>
2022-09-04 17:08:23 -0400

    Add files via upload

---
+++ practica d java/main.js
@@ -0,0 +1,21 @@
+//  console . log("estoy vivo")
+
+//const $ = (selector) => document.getelementty)
+
+const nombre = document.getElementById("nombre")
+
+
+
+
+
+
+
+
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click" , (e) => {
+e. preventDefault();
+alert(nombre.value);
+
+});
+
